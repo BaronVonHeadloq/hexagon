65928d55e3408cabce45dbc6f4d5b1dc2497da8c
BaronVonHeadloq <86290167+BaronVonHeadloq@users.noreply.github.com>
2022-05-31 10:09:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,10 @@ import pandas
 
 streamlit.title('Feed me, Seymour!')  
 
-streamlit.header(':-) A Header of Note')
-
-streamlit.text('ConTEXTual text, eh?')
+#streamlit.header(':-) A Header of Note')
+#streamlit.text('ConTEXTual text, eh?')
 
 my_fruit_list = pandas.read("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.text('Show and tell, Pandas')
-
-
-streamlit.dataframe(my_fruit_list)
+#streamlit.text('Show and tell, Pandas')
+#streamlit.dataframe(my_fruit_list)
